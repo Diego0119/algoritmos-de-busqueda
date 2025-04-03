bf315e068f7cffea308722f52d6f2258474a05cf
Diego0119 <diegosanhuezaaguilar@gmail.com>
2025-04-03 09:19:15 -0300

    bubblesort

---
+++ src/bubble_sort.c
@@ -0,0 +1,19 @@
+#include "header.h"
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int swapped = 0;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = 1;
+            }
+        }
+        if (swapped == 0)
+            break;
+    }
+}
--- src/swap.c
+++ src/swap.c
@@ -1,5 +1,16 @@
-#include "header.h"
+/**
+ * @file swap.c
+ * @brief archivo que contine la función swap
+ * @authors Felipe Carcamo, Duvan Figueroa, Diego Sanhueza
+ */
 
+#include "header.h" ///< Incluye la definición de las funciones
+/**
+ * @brief función para intercambiar elementos
+ *
+ * @param element_a entero a intercambiar a
+ * @param element_b entero a intercambiar b
+ */
 void swap(int *element_a, int *element_b)
 {
     int *temp = *element_a;
