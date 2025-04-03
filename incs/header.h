5d13871a87005452fab19e22eacb9811f644085a
Diego0119 <diegosanhuezaaguilar@gmail.com>
2025-04-03 09:32:59 -0300

    save

--- incs/header.h
+++ incs/header.h
@@ -1,3 +1,7 @@
 #include <stdio.h>
 
 void swap(int *element_a, int *element_b);
+
+void bubble_sort(int arr[], int n);
+
+void bubble_sort_op(int arr[], int n);
--- src/bubble_sort.c
+++ src/bubble_sort.c
@@ -1,11 +1,26 @@
 #include "header.h"
 
-void bubbleSort(int arr[], int n)
+void bubble_sort(int arr[], int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
-        int swapped = 0;
         for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+void bubble_sort_op(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int swapped = 0; // bandera que permite encontrar si el arreglo estaba ordenado desde antes
+
+        for (int j = 0; j < n - i - 1; j++) // n - i - 1 reduce el rango de busqueda, asi no recorre siempre hasta el final cuando el elemento ordenado ya subio al final
         {
             if (arr[j] > arr[j + 1])
             {
@@ -13,7 +28,8 @@ void bubbleSort(int arr[], int n)
                 swapped = 1;
             }
         }
-        if (swapped == 0)
+
+        if (!swapped)
             break;
     }
 }
