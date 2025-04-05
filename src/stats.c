ef5acd707ad0e08e014749a3a9582c0007d0f512
FeLipe-133 <fcaroyar@umag.cl>
2025-04-05 20:13:53 -0300

    implementando solo nombre de funciones para las estadisticas

---
+++ src/stats.c
@@ -0,0 +1,41 @@
+#include "header.h"
+//preguntar si las funciones las hacemos en ingles :P
+void total_stock()
+{
+
+}
+
+void valor_inventario() //suma de precio × stock por producto
+{
+
+}
+
+void mayor_stock()
+{
+
+}
+
+void menor_stock()
+{
+
+}
+
+void mas_caro()
+{
+
+}
+
+void menos_caro()
+{
+    
+}
+
+void promedio_por_categoria()
+{
+
+}
+
+void total_por_categoria()
+{
+    
+}
