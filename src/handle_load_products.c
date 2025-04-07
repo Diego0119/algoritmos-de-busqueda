99eb28dfbe6da756c9633e150dbc4d3159a4bb0c
Diego0119 <diegosanhuezaaguilar@gmail.com>
2025-04-07 20:16:43 -0300

    añadiendo opciones al binary search

--- src/handle_load_products.c
+++ src/handle_load_products.c
@@ -1,7 +1,7 @@
 #include "header.h"
 
 // Función para cargar productos
-void handle_load_products(Producto productos[], int *cantidad) 
+void handle_load_products(Producto productos[], int *cantidad)
 {
     *cantidad = load_products("data.csv", productos);
     printf("%d productos cargados.\n", *cantidad);
--- src/handle_search_products.c
+++ src/handle_search_products.c
@@ -1,6 +1,6 @@
 #include "header.h"
 
-void handle_search_products(Producto productos[], int cantidad) 
+void handle_search_products(Producto productos[], int cantidad)
 {
     int search_option;
 
@@ -8,36 +8,96 @@ void handle_search_products(Producto productos[], int cantidad)
     printf("1. Busqueda Secuencial\n");
     printf("2. Busqueda Binaria\n");
     printf("Opcion: ");
-    if (scanf("%d", &search_option) != 1) 
+    if (scanf("%d", &search_option) != 1)
     {
         fprintf(stderr, "Error: Entrada invalida.\n");
-        while (getchar() != '\n');
+        while (getchar() != '\n')
+            ;
         return;
     }
 
-    if (search_option == 1) 
+    if (search_option == 1)
     {
         int id;
         printf("Ingrese el ID del producto a buscar: ");
-        if (scanf("%d", &id) != 1) 
+        if (scanf("%d", &id) != 1)
         {
             fprintf(stderr, "Error: Entrada invalida.\n");
-            while (getchar() != '\n');
+            while (getchar() != '\n')
+                ;
             return;
         }
-
         int index = secuencial_search(productos, cantidad, id);
-        if (index != -1) 
+        if (index != -1)
         {
             printf("Producto encontrado: %s\n", productos[index].nombre);
-        } 
-        else 
+        }
+        else
         {
             printf("Producto no encontrado.\n");
         }
-    } 
-    else if (search_option == 2) 
+    }
+    else if (search_option == 2)
     {
-        printf("En proceso de implementacion...\n");
+        int binary_option;
+        insertion_sort_by_id(productos, cantidad);
+        printf("Seleccione el tipo de busqueda binaria:\n");
+        printf("1. Por ID\n");
+        printf("2. Por Nombre\n");
+        printf("Opcion: ");
+        if (scanf("%d", &binary_option) != 1)
+        {
+            fprintf(stderr, "Error: Entrada invalida.\n");
+            while (getchar() != '\n')
+                ;
+            return;
+        }
+
+        if (binary_option == 1)
+        {
+            int id;
+            printf("Ingrese el ID del producto a buscar: ");
+            if (scanf("%d", &id) != 1)
+            {
+                fprintf(stderr, "Error: Entrada invalida.\n");
+                while (getchar() != '\n')
+                    ;
+                return;
+            }
+            int index = recursive_binary_search_by_id(productos, 0, cantidad - 1, id);
+
+            if (index != -1)
+            {
+                printf("Producto encontrado: %s\n", productos[index].nombre);
+            }
+            else
+            {
+                printf("Producto no encontrado.\n");
+            }
+        }
+        else if (binary_option == 2)
+        {
+            char nombre[100];
+            printf("Ingrese el nombre del producto a buscar: ");
+            scanf("%s", nombre);
+
+            int index = recursive_binary_search_by_name(productos, 0, cantidad - 1, nombre);
+            if (index != -1)
+            {
+                printf("Producto encontrado: ID %d, Nombre: %s\n", productos[index].id, productos[index].nombre);
+            }
+            else
+            {
+                printf("Producto no encontrado.\n");
+            }
+        }
+        else
+        {
+            printf("Opcion invalida.\n");
+        }
+    }
+    else
+    {
+        printf("Opcion invalida.\n");
     }
 }
